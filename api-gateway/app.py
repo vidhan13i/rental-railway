888185392c6ee4cb60cd97d075fdf6cd39f9b6e5
Vidhan_Manihar <vidhanmanihar@gmail.com>
2026-03-24 15:43:52 +0530

    Update app.py

--- api-gateway/app.py
+++ api-gateway/app.py
@@ -156,5 +156,4 @@ def home():
 # ===================== RUN ===================== #
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
