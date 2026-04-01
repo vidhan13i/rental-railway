62dfc721af803dbc822b79fba85a8c41b3d347b9
vidhan13i <vidhanmanihar@gmail.com>
2026-04-01 23:28:09 +0530

    fixed port issue

--- bookings-service/app.py
+++ bookings-service/app.py
@@ -153,5 +153,5 @@ with app.app_context():
 # ---------------- RUN ----------------
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
--- listings-service/app.py
+++ listings-service/app.py
@@ -129,5 +129,5 @@ with app.app_context():
 # RUN APP
 # -------------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
